71157dd3ac9fe0a2d3013eda3c67de27847c2ac3
Kevin Garner <kevinleegarner@gmail.com>
2020-07-07 17:55:56 -0500

    Started sonar.py from Ch 13

---
+++ sonar/sonar.py
@@ -0,0 +1,22 @@
+# Sonar Treasure Hunt
+
+import random
+import sys
+import math
+
+def getNewBoard():
+    # Create a new 60x15 board data structure.
+    board = []
+    for x in range(60): # The main list is a list of 60 lists
+        board.append([])
+        for y in range(15): # Each list in the main list has 15 single-character strings.
+            # Use different characters for the ocean to make it more readable.
+            if random.randint(0, 1) == 0:
+                board[x].append('~')
+            else:
+                board[x].append('`')
+    return board
+
+def drawBoard(board):
+    # Draw the board data structure.
+    tensDigitsLine = '    ' # Initial space for the numbers down the left
